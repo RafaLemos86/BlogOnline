b39cdd47920ad7abed00eda6b3614130771fd7c0
Rafael <rfael.lemos@gmail.com>
2022-11-11 22:40:53 -0300

    tela newlogin

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router()
 const User = require('./User')
+const bcrypt = require('bcryptjs')
 
 
 router.get('/admin/users', (req, res) => {
-    res.send('Paginacao de usuarios')
-})
+    User.findAll().then((users) => {
+        res.render('admin/users/index', {
+            users: users
+        })
+    })
+});
+
 
 router.get('/admin/users/create', (req, res) => {
     res.render('admin/users/create')
@@ -16,12 +22,30 @@ router.post('/users/create', (req, res) => {
     var email = req.body.email
     var password = req.body.password
 
-    res.json({
-        email, password
+    // verificando se o email ja foi cadastrado
+    User.findOne({
+        where: {
+            email: email
+        }
+    }).then((user) => {
+        if (user == undefined) {
+
+            // variavel para aumentar ainda mais a seguranca
+            var salt = bcrypt.genSaltSync(10)
+            var hash = bcrypt.hashSync(password, salt);
+
+            User.create({
+                email: email,
+                password: hash
+            }).then(() => {
+                res.redirect('/')
+            }).catch(() => {
+                res.redirect('/')
+            })
+        } else {
+            res.redirect('/admin/users/create')
+        }
     })
 })
-
-
-
 
 module.exports = router
